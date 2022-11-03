26d28e4f33d9a9ad9f76998b5ccbad987362b41d
Carmen de Lope Contreras <carmen@napptive.com>
2022-11-03 14:57:25 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func main() {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
     str := "This is a Golang example\nPlease, select a valid repository changing the LAUNCHER_TARGET_REPO value"
+    https://github.com/Carmendelope/simple-app-go
     fmt.Fprintf(w, str)
 }
