2949ca5f6cfc13d807b5d21449f8af58eda4d6d0
Carmen de Lope Contreras <carmen@napptive.com>
2023-01-27 13:18:54 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@
 date >> date.txt
 
 ## Replace text
-sed 's/This is a Golang example/This is an example with a pre-build step/g'  ./main.go >> ./main.go
+sed 's/This is a Golang example/This is an example with a pre-build step/g'  ./main.go > ./main.go
 
